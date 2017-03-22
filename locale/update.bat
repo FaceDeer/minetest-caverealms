5363ae5d6ec2c8281fc8d1245aec37a8596b0d2c
FaceDeer <derksenmobile@gmail.com>
2017-03-22 00:02:45 -0600

    split subterrane out into a separate repository

--- locale/update.bat
+++ locale/update.bat
@@ -3,4 +3,4 @@ setlocal ENABLEEXTENSIONS ENABLEDELAYEDEXPANSION
 cd ..
 set LIST=
 for /r %%X in (*.lua) do set LIST=!LIST! %%X
-..\..\intllib\tools\xgettext.bat %LIST%
+..\intllib\tools\xgettext.bat %LIST%
